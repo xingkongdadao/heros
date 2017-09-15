90864c776611810c7eaad8a46756e742fa5952de
lijiang <lijiang@gogotrans.com>
2017-09-15 22:41:10 +0700

    英雄编辑器

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+// 添加英雄对象
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  // hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorms'
+  };
 }
+
