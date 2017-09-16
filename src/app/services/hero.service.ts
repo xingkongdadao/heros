8d2ee88971515a34d849b25fc6733dd6b386d5b0
lijiang <lijiang@gogotrans.com>
2017-09-16 18:15:21 +0700

    异步服务与承诺-模拟延迟加载效果

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -12,4 +12,12 @@ export class HeroService {
     return Promise.resolve(HEROES);
   }
 
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+
 }
