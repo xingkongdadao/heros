e5bf216f8d0487922b42f938e951533be8e6c286
lijiang <lijiang@gogotrans.com>
2017-09-16 16:58:06 +0700

    主从结构

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,23 @@ import { Component } from '@angular/core';
 export class Hero {
   id: number;
   name: string;
+
+
+
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'app-root',
@@ -18,5 +34,13 @@ export class AppComponent {
     id: 1,
     name: 'Windstorms'
   };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelected(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+
 }
 
